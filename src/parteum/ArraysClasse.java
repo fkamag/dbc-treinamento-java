99207a94479d9fbd8a0d0adbc88c7ec9bd0c11e4
fkamag <fabiokamagian@gmail.com>
2024-11-26 15:40:35 -0300

    feat Arrays Classe

---
+++ src/parteum/ArraysClasse.java
@@ -0,0 +1,34 @@
+package parteum;
+
+import java.util.Arrays;
+
+public class ArraysClasse {
+
+  public static void main(String[] args) {
+    int[] numeros = {1, 2, 3, 4, 5};
+    System.out.println(numeros[0]);
+
+    numeros[2] = 28;
+    System.out.println(Arrays.toString(numeros));
+
+    String[] ordenarArray = {"Pafúncio", "Esmeralda", "Ana Banana", "Cláudio",
+        "Zaire"};
+    System.out.println(Arrays.toString(ordenarArray));
+    Arrays.sort(ordenarArray);
+    System.out.println("Array ordenado: " + Arrays.toString(ordenarArray));
+
+
+    int[][] matriz = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for (int i = 0; i < matriz.length; i++) {
+      for (int j = 0; j < matriz[0].length; j++) {
+        System.out.print(matriz[i][j]);
+      }
+      System.out.println();
+    }
+  }
+}
